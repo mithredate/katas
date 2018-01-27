b8438a56c8f8a4994dece46a31151bc79022d7c9
Mehrdad Hedayati <mehrdad.hedayati@gmail.com>
2018-01-27 19:04:05 +0330

    Added getBoardDimension to Game

--- src/QueenAttack/Game.php
+++ src/QueenAttack/Game.php
@@ -10,11 +10,25 @@ namespace Mithredate\Hackerrank\QueenAttack;
 
 class Game
 {
+    /**
+     * @var Board
+     */
+    private $board;
 
     /**
      * Game constructor.
      */
     public function __construct()
     {
+    }
+
+    public function setBoard(Board $board)
+    {
+        $this->board = $board;
+    }
+
+    public function getBoardDimension()
+    {
+        return $this->board->getDimension();
     }
 }
--- tests/QueenAttack/GameTest.php
+++ tests/QueenAttack/GameTest.php
@@ -7,16 +7,19 @@
 
 namespace Mithredate\Hackerrank\QueenAttack;
 
-
+use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
 class GameTest extends TestCase
 {
 
-    public function testNewGame()
+    public function testSetDimensionOnGame()
     {
         $game = new Game();
-        $this->assertInstanceOf(Game::class, $game);
+        $game->setBoard(m::mock(Board::class, function($mock) {
+            $mock->shouldReceive('getDimension')->once()->andReturn(8);
+        }));
+        $this->assertEquals(8, $game->getBoardDimension());
     }
 
 }
