6c97ec14acd1c3ebb93e6594ed8106a604944bbb
Mehrdad Hedayati <mehrdad.hedayati@gmail.com>
2018-01-27 21:42:39 +0330

    tested move top with obstacles

--- tests/QueenAttack/GameTest.php
+++ tests/QueenAttack/GameTest.php
@@ -103,6 +103,18 @@ class GameTest extends TestCase
         $this->assertEquals(5, $this->game->numberOfValidCellsToMoveUp());
     }
 
+    public function testMoveUpWithObstacle()
+    {
+        $this->mockQueenAt(5, 2);
+
+        $board = $this->getBoardMockWithDimension(8);
+        $board->shouldReceive('hasObstacle')->once()->withArgs([8, 2])->andReturn(true);
+        $board->shouldReceive('hasObstacle')->times(5)->withAnyArgs()->andReturn(false);
+        $this->game->setBoard($board);
+
+        $this->assertEquals(2, $this->game->numberOfValidCellsToMoveUp());
+    }
+
     /**
      * @param $row
      * @param $col
