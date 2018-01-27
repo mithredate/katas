fe703f419afe5f157ff43805f0c87524e0043d19
Mehrdad Hedayati <mehrdad.hedayati@gmail.com>
2018-01-27 22:07:20 +0330

    Tested move downward on secondary diagonal with obstacles

--- tests/QueenAttack/GameTest.php
+++ tests/QueenAttack/GameTest.php
@@ -218,6 +218,18 @@ class GameTest extends TestCase
         $this->assertEquals(1, $this->game->numberOfValidCellsToMoveOnSecondaryDiagonalDownward());
     }
 
+    public function testMoveDownwardOnSecondaryDiagonalWithObstacles()
+    {
+        $this->mockQueenAt(6, 5);
+
+        $board = $this->getBoardMockWithDimension(8);
+        $board->shouldReceive('hasObstacle')->once()->withArgs([3, 2])->andReturn(true);
+        $board->shouldReceive('hasObstacle')->times(3)->withAnyArgs()->andReturn(false);
+        $this->game->setBoard($board);
+
+        $this->assertEquals(2, $this->game->numberOfValidCellsToMoveOnSecondaryDiagonalDownward());
+    }
+
     /**
      * @param $row
      * @param $col
