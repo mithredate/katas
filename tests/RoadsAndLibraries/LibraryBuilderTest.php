be7710aca9fb777679caa34516ce9df6e369d0e7
Mehrdad Hedayati <mehrdad.hedayati@rdsysco.com>
2018-01-30 16:22:59 +0330

    Tested total cost on a complex case

--- tests/RoadsAndLibraries/LibraryBuilderTest.php
+++ tests/RoadsAndLibraries/LibraryBuilderTest.php
@@ -32,4 +32,26 @@ class LibraryBuilderTest extends RoadsAndLibrariesTestCase
 
         $this->assertEquals(4, $libraryBuilder->getTotalCost());
     }
+
+    public function testTotalCostForAComplexHackerLand()
+    {
+        $hackerLand = m::mock(HackerLand::class);
+        $city1 = $this->getCityMockWithNumber(1);
+        $city2 = $this->getCityMockWithNumber(2);
+        $city3 = $this->getCityMockWithNumber(3);
+        $city4 = $this->getCityMockWithNumber(4);
+        $city5 = $this->getCityMockWithNumber(5);
+        $city6 = $this->getCityMockWithNumber(6);
+        $city7 = $this->getCityMockWithNumber(7);
+        $city8 = $this->getCityMockWithNumber(8);
+
+        $hackerLand->shouldReceive('getAdjacentCities')->once()->andReturn([
+            [1 => $city1, 2 => $city2, 3 => $city3, 7 => $city7],
+            [5 => $city5, 6 => $city6, 8 => $city8]
+        ]);
+
+        $libraryBuilder = new LibraryBuilder($hackerLand, 5, 2);
+
+        $this->assertEquals(20, $libraryBuilder->getTotalCost());
+    }
 }
